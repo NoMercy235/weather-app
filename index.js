987be413c79534fd830de5e2d7efab360570a45f
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-03-06 10:07:54 +0200

    Changed index.js to allow the app to be used either as a script or as an npm package.

--- index.js
+++ index.js
@@ -1,38 +1,66 @@
-const yargs = require('yargs');
 const fs = require('fs');
-
-if (!fs.existsSync('env.js')) {
-    console.error('Environment file not detected. Please create "./env.js".');
-    process.exit(1)
-}
-
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
-const argv = yargs
-    .options({
-        address: {
-            alias: 'a',
-            demand: true,
-            describe: 'Address for which you request the weather.',
-            string: true,
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+const runningAsScript = !module.parent;
 
-geocode.geocodeAddress(argv.address)
-    .then((geocodeData) => {
-        console.log(JSON.stringify(geocodeData, null, 4));
-        weather.weatherForCity(geocodeData).then((data) => {
-            console.log(JSON.stringify(data, null, 4));
-            process.send({
-                geocode: geocodeData,
-                weather: data,
+// To be used if this is ran as a script;
+let yargs;
+let env;
+
+if (runningAsScript) {
+    if (!fs.existsSync('env.js')) {
+        console.error('Environment file not detected. Please create "./env.js".');
+        process.exit(1);
+    } else {
+        env = require('./env.js');
+    }
+    yargs = require('yargs');
+}
+
+let argv;
+
+if (runningAsScript) {
+    argv = yargs
+        .options({
+            address: {
+                alias: 'a',
+                demand: true,
+                describe: 'Address for which you request the weather.',
+                string: true,
+            }
+        })
+        .help()
+        .alias('help', 'h')
+        .argv;
+}
+
+function getWeatherForCity(keys) {
+    geocode.geocodeAddress(argv.address, keys.googleApiKey)
+        .then((geocodeData) => {
+            console.log(JSON.stringify(geocodeData, null, 4));
+            weather.weatherForCity(geocodeData, keys.forecastApiKey).then((data) => {
+                console.log(JSON.stringify(data, null, 4));
+                process.send && process.send({
+                    geocode: geocodeData,
+                    weather: data,
+                });
+            }).catch((err) => {
+                console.error(err);
             });
         }).catch((err) => {
-            console.error(err);
-        });
-    }).catch((err) => {
         console.error(err);
     });
+}
+
+if (runningAsScript) {
+    getWeatherForCity({
+        googleApiKey: env.googleApiKey,
+        forecastApiKey: env.forecastApiKey,
+    });
+}
+
+module.exports = {
+    geocode: geocode,
+    weather: weather,
+    getWeatherForCity: getWeatherForCity
+};
